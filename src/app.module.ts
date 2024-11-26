71cfdc9352657e79923ec53bf14030278feefe27
c3ulnta0rk <dogevi@gmail.com>
2024-11-26 13:56:00 +0000

    Add static files dir

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './modules/auth/entities/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      entities: [UserEntity],
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,25 @@ import {
 import { AppModule } from './app.module';
 import secureSession from '@fastify/secure-session';
 import { Logger } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
   await app.register(secureSession, {
     secret: 'averylogphrasebiggerthanthirtytwochars',
     salt: 'mq9hDxBVDbspDR6n',
   });
+
+  const staticFilePath = join(__dirname, '..', 'public');
+  app.useStaticAssets({
+    root: staticFilePath,
+    prefix: '/public/',
+  });
+  Logger.log(`App serve the files from the path ${staticFilePath}`);
 
   await app.listen(process.env.PORT ?? 3000);
   Logger.log('App listening : http://localhost:3000');
